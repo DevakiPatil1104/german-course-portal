4f5a9522b7d46856346ef7f447924f0786382c6a
Devaki Patil <devakipatil7@gmail.com>
2026-06-23 15:33:44 +0530

    Added ScrollToTop functionality

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -3,10 +3,12 @@ import Navbar from '../components/common/Navbar'
 import Footer from '../components/common/Footer'
 import WhatsAppButton from '../components/common/WhatsAppButton'
 import CourseFinder from '../components/common/courseFinder'
+import ScrollToTop from '../components/common/ScrollToTop'
 
 function MainLayout() {
   return (
     <div className="bg-page-glow flex min-h-screen flex-col">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-1">
         <Outlet />
